aa9cbe324825008b7a72751a2f9f55ab9699f7b8
gonmagallanes <magallanesgonzalonicolas@gmail.com>
2024-07-11 16:08:42 -0300

    hola jueves

--- Program.cs
+++ Program.cs
@@ -20,42 +20,68 @@ class Program
         
             switch(oDatos.eleccion){
                 case 1: 
-                    
+
                     Console.WriteLine("Introducir nombre del primer alumno");
                     oDatos.nombreAlumno1 = Console.ReadLine();
-                    Console.WriteLine("Introducir nota del primer alumno");
-                    oDatos.notaAlumno1 = int.Parse(Console.ReadLine());                
 
-                    if (oDatos.notaAlumno1 < 0 || oDatos.notaAlumno1 > 10) {
-                        do {
-                            Console.WriteLine("Por favor introducir una nota del 0 al 10");
-                            oDatos.notaAlumno1 = int.Parse(Console.ReadLine());         
-                        }while(oDatos.notaAlumno1 < 0 || oDatos.notaAlumno1 > 10);
-                    }                          
+                    try {
+                        
+                        Console.WriteLine("Introducir nota del primer alumno");
+                        oDatos.notaAlumno1 = int.Parse(Console.ReadLine());                
+
+                        if (oDatos.notaAlumno1 < 0 || oDatos.notaAlumno1 > 10) {
+                            do {
+                                Console.WriteLine("Por favor introducir una nota del 0 al 10");
+                                oDatos.notaAlumno1 = int.Parse(Console.ReadLine());         
+                            }while(oDatos.notaAlumno1 < 0 || oDatos.notaAlumno1 > 10);
+                        }   
+                    }catch{
+                        Console.WriteLine("Definir nota con numeros, nota definida temporalmente como 1(uno)");
+                        oDatos.notaAlumno1 = 1;
+                    }
+                                           
                               
                     Console.WriteLine("Introducir nombre del segundo alumno");
                     oDatos.nombreAlumno2 = Console.ReadLine();
-                    Console.WriteLine("Introducir nota del segundo alumno");
-                    oDatos.notaAlumno2 = int.Parse(Console.ReadLine());
 
-                    if (oDatos.notaAlumno2 < 0 || oDatos.notaAlumno2 > 10) {
-                        do {
-                            Console.WriteLine("Por favor introducir una nota del 0 al 10");
-                            oDatos.notaAlumno2 = int.Parse(Console.ReadLine());         
-                        }while(oDatos.notaAlumno2 < 0 || oDatos.notaAlumno2 > 10);
+                    try {
+                        Console.WriteLine("Introducir nota del segundo alumno");
+                        oDatos.notaAlumno2 = int.Parse(Console.ReadLine());
+
+                        if (oDatos.notaAlumno2 < 0 || oDatos.notaAlumno2 > 10) {
+                            do {
+                                Console.WriteLine("Por favor introducir una nota del 0 al 10");
+                                oDatos.notaAlumno2 = int.Parse(Console.ReadLine());         
+                            }while(oDatos.notaAlumno2 < 0 || oDatos.notaAlumno2 > 10);
+                        }
+                    }catch{
+                        Console.WriteLine("Definir nota con numeros, nota definida temporalmente como 1(uno)");
+                        oDatos.notaAlumno2 = 1;
                     }
+                    
 
                     Console.WriteLine("Introducir nombre del tercer alumno");
                     oDatos.nombreAlumno3 = Console.ReadLine();
-                    Console.WriteLine("Introducir nota del tercer alumno");
-                    oDatos.notaAlumno3 = int.Parse(Console.ReadLine());
 
-                    if (oDatos.notaAlumno3 < 0 || oDatos.notaAlumno3 > 10) {
-                        do {
-                            Console.WriteLine("Por favor introducir una nota del 0 al 10");
-                            oDatos.notaAlumno3 = int.Parse(Console.ReadLine());         
-                        }while(oDatos.notaAlumno3 < 0 || oDatos.notaAlumno3 > 10);
+                    try{
+
+                        Console.WriteLine("Introducir nota del tercer alumno");
+                        oDatos.notaAlumno3 = int.Parse(Console.ReadLine());
+
+                        if (oDatos.notaAlumno3 < 0 || oDatos.notaAlumno3 > 10) {
+                            do {
+                                Console.WriteLine("Por favor introducir una nota del 0 al 10");
+                                oDatos.notaAlumno3 = int.Parse(Console.ReadLine());         
+                            }while(oDatos.notaAlumno3 < 0 || oDatos.notaAlumno3 > 10);
+                        }
+
+                    }catch{
+
+                        Console.WriteLine("Definir nota con numeros, nota definida temporalmente como 1(uno)");
+                        oDatos.notaAlumno3 = 1;
+                        
                     }
+                    
 
                     break;
                 case 2: 
@@ -64,7 +90,7 @@ class Program
                 case 3: 
                     Console.WriteLine("La nota promedio es de: " + oDatos.notaPromedio());
                     break;
-                default: 
+                case 4: 
                     Console.Beep(659, 125); 
                     Console.Beep(659, 125); 
                     Thread.Sleep(125); 
